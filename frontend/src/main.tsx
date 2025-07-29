953c7b60291c2c5523423708d08258727f76fb78
Nicolas Chambon <nicolaschambon@Mac.lan>
2025-07-29 16:32:54 +0200

    add appolo client provider and codegen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: `${import.meta.env.VITE_API_URL_FROM_CLIENT}/`,
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
