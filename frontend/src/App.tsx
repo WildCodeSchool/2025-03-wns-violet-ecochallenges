4506bb200a93a03f6e6d46d5c3736a607e41bc38
Nicolas Chambon <nicolaschambon@Mac.lan>
2025-07-29 15:32:50 +0200

    install vite, frontend dependenciesand start setup tailwind/shadcn (still in progress)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button className="text-foreground">COUCOU les loulous</Button>
+      <div className="bg-background h-32">COUCOU les loustiques</div>
+    </div>
+  );
+}
+
+export default App;
