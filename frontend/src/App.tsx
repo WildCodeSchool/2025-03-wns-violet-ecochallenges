414ac023a9128c97dfaf5f2817b86902703d863c
Alexandre Clasquin <alexandre.clasquin@gmail.com>
2025-09-12 14:59:14 +0200

    adding content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,11 @@
+import Banner from "../src/pages/Homepage/Banner";
+
 // import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div>
-      {/* <Button className="bg-background">COUCOU les loulous</Button> */}
-      <div className="bg-background font-sansitaOne">COUCOU les zerzer</div>
-      <div className="font-sansitaOne bg-red-50">
-        dfbsdhb CAPTAIN PLANET YOUHOUUUU
-      </div>
-      <div>dfbsdhb CAPTAIN PLANET YOUHOUUUU</div>
-
-      <div className="font-urbanist bg-blue-50">
-        dfbsdhb CAPTAIN PLANET YOUHOUUUU
-      </div>
-      <div>dfbsdhb CAPTAIN PLANET YOUHOUUUU</div>
-
-      <div className="font-inter bg-purple-500">
-        dfbsdhb CAPTAIN PLANET YOUHOUUUU
-      </div>
+      <Banner />
     </div>
   );
 }
--- frontend/src/pages/Homepage/Banner.tsx
+++ frontend/src/pages/Homepage/Banner.tsx
@@ -1,7 +1,29 @@
+import { Button } from "../../components/ui/Button";
+import { TypographyH2 } from "../../components/ui/TypographyH2";
+import crewPlanetLogo from "../../../public/crew-planet.png"
+
 function Banner() {
     return (
-        <h1> Lance ton challenge écolo</h1>
+        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between p-4">
+            {/* Contenu texte et boutons */}
+            <div className="flex flex-col space-y-4 lg:flex-1">
+                <TypographyH2>Lance ton challenge écolo !</TypographyH2>
+                <p className="text-gray-600">
+                    Crée un challenge, invite ton groupe et passe à l'action, un éco-geste à la fois.
+                </p>
+                <div className="flex flex-col space-y-3 lg:flex-row lg:space-y-0 lg:space-x-4">
+                    <Button>S'inscrire</Button>
+                    <Button className="bg-secondary">Se connecter</Button>
+                </div>
+            </div>
+            
+            <img 
+                src={crewPlanetLogo} 
+                alt="Crew Planet" 
+                className="hidden lg:block lg:flex-1 lg:max-w-md lg:ml-8"
+            />
+        </div>
     );
-};
+}
 
 export default Banner;
