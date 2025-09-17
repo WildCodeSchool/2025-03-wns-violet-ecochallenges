06574c0349c523101645294ca1263027d61c19df
NicolasChambon <nicolas.chambon.dev@gmail.com>
2025-09-17 15:13:00 +0200

    add resolve property and right config in vite.config.ts file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-// import { Button } from "@/components/ui/button";
-
 import { Outlet } from "react-router";
 
 function App() {
   return (
-  <div className="bg-background">
-    <Outlet />; 
-  </div>)
+    <div className="bg-background">
+      <Outlet />;
+    </div>
+  );
 }
 
 export default App;
