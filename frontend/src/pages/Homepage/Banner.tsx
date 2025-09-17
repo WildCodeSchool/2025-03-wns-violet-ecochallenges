22ab4139068868db203a3c37f2cba815543f1c12
Alexandre Clasquin <alexandre.clasquin@gmail.com>
2025-09-17 15:08:48 +0200

    Adding review's corrections

--- frontend/src/pages/Homepage/Banner.tsx
+++ frontend/src/pages/Homepage/Banner.tsx
@@ -1,41 +1,70 @@
-import { Button } from "../../components/ui/Button";
+import { Button } from "../../components/ui/button";
 import { TypographyH2 } from "../../components/ui/typographyH2";
-import { TypographyP } from "../../components/ui/TypographyP";
+import { TypographyP } from "../../components/ui/typographyP";
 import { Link } from "react-router";
+import { cn } from "../../lib/utils";
 
 function Banner() {
   return (
-    <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between p-4 lg:p-8">
-      {/* Section contenu principal */}
-      <div className="flex flex-col space-y-4 lg:flex-1">
+    <div 
+      className={cn(
+        "flex flex-col",
+        "lg:flex-row",
+        "lg:items-center",
+        "lg:justify-between",
+        "p-4",
+        "lg:p-8"
+        )}
+      >
+      <div className={cn("flex",
+        "flex-col",
+        "space-y-4",
+        "lg:flex-1"
+        )}
+        >
         <TypographyH2>Lance ton challenge écolo !</TypographyH2>
         
         <TypographyP>
           Crée un challenge, invite ton groupe et passe à l'action, un éco-geste à la fois.
         </TypographyP>
         
-        {/* Section boutons d'action */}
-        <div className="flex flex-col space-y-3 lg:flex-row lg:space-y-0 lg:gap-5 lg:w-full">
-          {/* //TODO Définir la route "Signup" */}
-          <Link to="#" className="lg:flex-1">
-            <Button className="w-full">S'inscrire</Button>
-          </Link>
+        <div className={cn(
+          "flex",
+          "flex-col",
+          "items-center",
+          "space-y-3",
+          "sm:flex-row",
+          "sm:justify-center",
+          "sm:space-y-0",
+          "sm:gap-5",
+          "lg:justify-start"
+        )}
+        >
+          <Button asChild className={(
+            "w-[200px]"
+            )}
+            >
+            <Link to="/signin">S'inscrire</Link>
+          </Button>
 
-          {/* //TODO Définir la route "Signin" */}
-          <Link to="#" className="lg:flex-1">
-            <Button variant="secondary" className="w-full text-secondary-foreground text-white">
-              Se connecter
-            </Button>
-          </Link>
+          <Button asChild variant="secondary" className={cn(
+            "w-[200px]"
+            )}>
+            <Link to="/signup">Se connecter</Link>
+          </Button>
         </div>
-        
       </div>
       
-      {/* Image visible uniquement sur desktop */}
       <img 
         src="/crew-planet.png" 
         alt="Crew Planet - Illustration challenge écologique" 
-        className="hidden lg:block lg:flex-1 lg:max-w-md lg:ml-8"
+        className={cn(
+          "hidden", 
+          "lg:block", 
+          "w-[150px]",
+          "h-auto", 
+          "ml-8"
+        )}
       />
     </div>
   );
