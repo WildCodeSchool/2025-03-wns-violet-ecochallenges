66b33be7dc57610e890fc92d79daf2a050ab1016
Alexandre Clasquin <alexandre.clasquin@gmail.com>
2025-10-30 10:13:57 +0100

    Adding style to login module

--- frontend/src/components/ui/login.tsx
+++ frontend/src/components/ui/login.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 
 interface LoginProps {
   heading?: string;
@@ -13,59 +14,52 @@ interface LoginProps {
   googleText?: string;
   signupText?: string;
   signupUrl?: string;
+  bg?: string;
 }
 
 const Login = ({
-  heading = "Login",
-  logo = {
-    url: "https://www.shadcnblocks.com",
-    src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-wordmark.svg",
-    alt: "logo",
-    title: "shadcnblocks.com",
-  },
-  buttonText = "Login",
-  signupText = "Need an account?",
+  heading = "CREER UN COMPTE",
+  buttonText = "S'inscrire",
   signupUrl = "https://shadcnblocks.com",
+  bg = "bg-white"
 }: LoginProps) => {
   return (
     <section className="bg-muted h-screen">
       <div className="flex h-full items-center justify-center">
-        {/* Logo */}
         <div className="flex flex-col items-center gap-6 lg:justify-start">
-          <a href={logo.url}>
-            <img
-              src={logo.src}
-              alt={logo.alt}
-              title={logo.title}
-              className="h-10 dark:invert"
-            />
-          </a>
-          <div className="min-w-sm border-muted bg-background flex w-full max-w-sm flex-col items-center gap-y-4 rounded-md border px-6 py-8 shadow-md">
+          <div className={cn(bg, "min-w-sm border-muted {bg} flex w-full max-w-sm flex-col items-center gap-y-4 rounded-md border px-6 py-8 shadow-md")}>
             {heading && <h1 className="text-xl font-semibold">{heading}</h1>}
+            <div className="text-muted-foreground flex justify-center gap-1 text-sm">
+              <a
+                href={signupUrl}
+                className="text-primary font-medium hover:underline"
+              >
+                J'ai déjà un compte
+              </a>
+            </div>
+            <label
+              className="text-sm self-start">
+              Email:
+            </label>
             <Input
               type="email"
               placeholder="Email"
-              className="text-sm"
+              className={cn(bg,"text-sm bg-white")}
               required
             />
+            <label
+              className="text-sm self-start">
+              Mot de passe:
+            </label>
             <Input
               type="password"
-              placeholder="Password"
-              className="text-sm"
+              placeholder="Mot de passe"
+              className={cn(bg,"text-sm bg-white")}
               required
             />
             <Button type="submit" className="w-full">
               {buttonText}
             </Button>
-          </div>
-          <div className="text-muted-foreground flex justify-center gap-1 text-sm">
-            <p>{signupText}</p>
-            <a
-              href={signupUrl}
-              className="text-primary font-medium hover:underline"
-            >
-              Sign up
-            </a>
           </div>
         </div>
       </div>
