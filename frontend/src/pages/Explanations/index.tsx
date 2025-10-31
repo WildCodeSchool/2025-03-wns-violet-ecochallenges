094be9d74abb00df8cc163147a08951f298d23d7
Alexandre Clasquin <alexandre.clasquin@gmail.com>
2025-10-31 15:27:13 +0100

    Adding max width for Explanations component

--- frontend/src/pages/Explanations/index.tsx
+++ frontend/src/pages/Explanations/index.tsx
@@ -4,48 +4,52 @@ import Arrow from "@/pages/Explanations/Arrow.tsx";
 
 function Explanations() {
   return (
-    <section className="font-urbanist md:bg-secondary py-8 max-w-8xl m-auto ">
-      <TypographyH2 className="text-white p-4 mb-8">
-        Comment ça marche ?
-      </TypographyH2>
+    <section className="md:bg-secondary">
+      <div className="container mx-auto w-full max-w-7xl">
+          <div  className="font-urbanist py-8">
+            <TypographyH2 className="text-white p-4 mb-8">
+              Comment ça marche ?
+            </TypographyH2>
 
-      <div className="flex flex-col lg:flex-row items-center lg:justify-center gap-4 lg:gap-8 px-4">
-        <div className="relative">
-          <ExplanationItem
-            number={1}
-            content={"Je crée un challenge"}
-            displayDirection="left"
-          />
+            <div className="flex flex-col lg:flex-row items-center lg:justify-center gap-4 lg:gap-8 px-4">
+              <div className="relative">
+                <ExplanationItem
+                  number={1}
+                  content={"Je crée un challenge"}
+                  displayDirection="left"
+                />
 
-          <Arrow
-            type="curved"
-            className="absolute -right-[4.3rem] top-5 lg:hidden"
-            direction="right"
-          />
-        </div>
-        <Arrow type="straight" className="hidden lg:block" />
+                <Arrow
+                  type="curved"
+                  className="absolute -right-[4.3rem] top-5 lg:hidden"
+                  direction="right"
+                />
+              </div>
+              <Arrow type="straight" className="hidden lg:block" />
 
-        <div className="relative">
-          <ExplanationItem
-            number={2}
-            content={"J'ajoute des éco-gestes"}
-            displayDirection="left"
-          />
+              <div className="relative">
+                <ExplanationItem
+                  number={2}
+                  content={"J'ajoute des éco-gestes"}
+                  displayDirection="left"
+                />
 
-          <Arrow
-            type="curved"
-            className="absolute -left-[4.3rem] top-[1rem] lg:hidden"
-            direction="left"
-          />
-        </div>
+                <Arrow
+                  type="curved"
+                  className="absolute -left-[4.3rem] top-[1rem] lg:hidden"
+                  direction="left"
+                />
+              </div>
 
-        <Arrow type="straight" className="hidden lg:block" />
+              <Arrow type="straight" className="hidden lg:block" />
 
-        <ExplanationItem
-          number={3}
-          content={"J'invite mes amis"}
-          displayDirection="left"
-        />
+              <ExplanationItem
+                number={3}
+                content={"J'invite mes amis"}
+                displayDirection="left"
+              />
+            </div>
+          </div>
       </div>
     </section>
   );
