e475fe18120f26027272c52e28459ea24a7e6801
Alexandre Clasquin <alexandre.clasquin@gmail.com>
2025-10-31 11:34:18 +0100

    Clean css and loading variable

--- frontend/src/pages/Registerpage/Index.tsx
+++ frontend/src/pages/Registerpage/Index.tsx
@@ -4,22 +4,22 @@ import { useNavigate } from "react-router";
 
 export default function SignupPage() {
 
-  const { signup, loading, error } = useSignup();
+  const { signup, error } = useSignup();
   const navigate = useNavigate();
   const handleSubmit = async (data: { email: string; password: string }) => {
 
     const ok = await signup(data.email, data.password);
 
     if (ok) {
-      navigate("/", { replace: true });
+      navigate("/", { replace: true }); // Redirection si await signup est true
     }
 
   };
 
   return (
 
-    <div className="min-h-screen flex items-center justify-center">
-      <Register onSubmit={handleSubmit} loading={loading} error={error ?? undefined} />
+    <div className="flex items-center justify-center mt-10 mb-10">
+      <Register onSubmit={handleSubmit} error={error ?? undefined} />
     </div>
 
   );
