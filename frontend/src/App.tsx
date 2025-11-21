f9bb91f82973b4f76a0c5cb8f3a70f0dc3bf2bfc
NicolasChambon <nicolas.chambon.dev@gmail.com>
2025-11-21 16:21:26 +0100

    add and use useScrollToTopOnRouteChange hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
 import Header from "./components/custom/Header/index.tsx";
 import { Footer } from "./components/custom/Footer";
+import { useScrollToTopOnRouteChange } from "./hooks/useScrollToTopOnRouteChange";
 
 function App() {
+  useScrollToTopOnRouteChange();
   return (
     <div className="bg-background flex flex-col min-h-screen">
       <Header />
