a74cc8d578232b696e500344c5ac5e7609ed5381
Célia <celiabillaud2@gmail.com>
2026-03-03 16:50:25 +0100

    clear apollo cache after logout

--- frontend/src/components/custom/Header/useAuthMenuActions.ts
+++ frontend/src/components/custom/Header/useAuthMenuActions.ts
@@ -1,6 +1,7 @@
 import { useLogoutMutation } from "@/generated/graphql-types";
 import { useNavigate } from "react-router";
 import { useAuthStore } from "@/stores/authStore";
+import { useApolloClient } from "@apollo/client";
 
 export function useAuthMenuActions() {
   const [logout] = useLogoutMutation();
@@ -8,11 +9,17 @@ export function useAuthMenuActions() {
   const isConnected = useAuthStore((state) => state.isConnected);
   const logoutStore = useAuthStore((state) => state.logout);
   const userPictureUrl = useAuthStore((state) => state.user?.pictureUrl);
+  const client = useApolloClient();
 
   const handleLogout = async () => {
-    await logout();
-    logoutStore();
-    navigate("/");
+    try {
+      await logout();
+      await client.clearStore();
+      logoutStore();
+      navigate("/");
+    } catch (error) {
+      console.error("Erreur:", error);
+    }
   };
 
   return { isConnected, handleLogout, userPictureUrl };
